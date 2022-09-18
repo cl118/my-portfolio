60c95ce5541015ef549d26862f77a6bd15b9d642
Chris Le <le.chrisv@gmail.com>
2022-09-18 01:36:44 -0400

    fixed text in contact links

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,23 +8,23 @@ const Contact = () => {
       <div className='bg-[#f7f7f7] rounded-[40px] lg:rounded-[90px]'>
         <div className='w-full max-w-[1170px] mx-auto flex flex-col lg:flex-row py-10 lg:py-0'>
           {/* Left Side */}
-          <div className='w-full lg:w-1/2 flex flex-col justify-center pr-4 lg:pr-0 pl-4'>
+          <div className='flex flex-col justify-center w-full pl-4 pr-4 lg:w-1/2 lg:pr-0'>
             <div className='relative max-w-[440px]'>
               <img
                 src='./dot-grid-accent.svg'
                 alt='/'
                 className='absolute top-0 right-0 h-[160px]'
               />
-              <h3 className='text-2xl font-thin leading-none tracking-wider my-6'>CONTACT</h3>
-              <h2 className='text-5xl font-extrabold tracking-wide mb-10'>Get In Touch </h2>
-              <p className='text-lg font-thin mb-8'>
+              <h3 className='my-6 text-2xl font-thin leading-none tracking-wider'>CONTACT</h3>
+              <h2 className='mb-10 text-5xl font-extrabold tracking-wide'>Get In Touch </h2>
+              <p className='mb-8 text-lg font-thin'>
                 Thank you for checking out my portfolio! Please send me a message if you have any
                 questions. I look forward to hearing from you!
               </p>
             </div>
           </div>
           {/* Right Side */}
-          <div className='w-full lg:w-1/2 flex items-center justify-end lg:my-40 pl-4 lg:pl-0 pr-4'>
+          <div className='flex items-center justify-end w-full pl-4 pr-4 lg:w-1/2 lg:my-40 lg:pl-0'>
             <ContactForm />
           </div>
         </div>
@@ -32,21 +32,21 @@ const Contact = () => {
 
       <div className='w-full max-w-[1170px] text-[#fdfdff] mx-auto flex flex-col lg:flex-row mt-16'>
         {/* Left Side */}
-        <div className='w-full lg:w-1/2 flex flex-col justify-center pr-4 lg:pr-0 pl-4'>
+        <div className='flex flex-col justify-center w-full pl-4 pr-4 lg:w-1/2 lg:pr-0'>
           <div className='relative max-w-[440px]'>
-            <h2 className='text-5xl font-extrabold tracking-wide mb-4'>Important Links</h2>
+            <h2 className='mb-4 text-5xl font-extrabold tracking-wide'>Important Links</h2>
             <p className='text-lg font-thin'>
-              Give my Github a peek and connect with me on LinkedIn. Liked what you saw? Shoot me
-              email and let's work together!
+              Give my Github a peek and connect with me on LinkedIn. Liked what you saw? Shoot me an
+              email and talk further!
             </p>
           </div>
         </div>
         {/* Right Side */}
-        <div className='w-full lg:w-1/2 relative flex items-center justify-center my-36 pl-4 lg:pl-0 pr-4'>
+        <div className='relative flex items-center justify-center w-full pl-4 pr-4 lg:w-1/2 my-36 lg:pl-0'>
           <div className='relative flex items-center gap-4 z-10 text-[#393d3f]'>
             <div className='h-[110px] w-[110px] lg:h-[128px] lg:w-[128px] bg-[#f9dbb3] rounded-lg flex justify-center items-center shadow-lg'>
               <a href='https://github.com/cl118' target='_blank'>
-                <FaGithub className='w-12 h-12 hover:text-green-900 rounded-full active:scale-95' />
+                <FaGithub className='w-12 h-12 rounded-full hover:text-green-900 active:scale-95' />
               </a>
             </div>
             <div className='h-[110px] w-[110px] lg:h-[128px] lg:w-[128px] bg-[#f9dbb3] rounded-lg flex justify-center items-center shadow-lg'>
