08d366450d145f17bcb928553117867d999f7e73
Chris Le <le.chrisv@gmail.com>
2022-09-18 04:29:03 -0400

    adjusted design tools icon sizes

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -37,33 +37,33 @@ const Skills = () => {
           </div>
         </div>
       </div>
-      <h2 className='hidden pt-24 text-5xl font-semibold text-center text-white lg:block'>
+      <h2 className='hidden pt-12 text-4xl font-semibold text-center text-white lg:block'>
         Design and Editing Tools
       </h2>
-      <div className='hidden mt-12 mb-16 max-w-[1170px] w-full mx-auto lg:flex flex-col lg:flex-row justify-around text-[#fdfdff]'>
+      <div className='hidden mt-12 mb-16 max-w-[750px] w-full mx-auto lg:flex flex-col lg:flex-row justify-around text-[#fdfdff]'>
         <div className='max-w-[250px] flex flex-col justify-center items-center text-center'>
-          <div className='h-[110px] w-[110px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
+          <div className='h-[90px] w-[90px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
             <SiAdobephotoshop className='w-12 h-12' />
           </div>
-          <h4 className='mb-5 text-xl'>Photoshop</h4>
+          <h4 className='mb-5 text-lg'>Photoshop</h4>
         </div>
         <div className='max-w-[250px] flex flex-col justify-center items-center text-center'>
-          <div className='h-[110px] w-[110px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
+          <div className='h-[90px] w-[90px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
             <SiAdobepremierepro className='w-12 h-12' />
           </div>
-          <h4 className='mb-5 text-xl'>Premiere Pro</h4>
+          <h4 className='mb-5 text-lg'>Premiere Pro</h4>
         </div>
         <div className='max-w-[250px] flex flex-col justify-center items-center text-center'>
-          <div className='h-[110px] w-[110px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
+          <div className='h-[90px] w-[90px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
             <SiAdobeaftereffects className='w-12 h-12' />
           </div>
-          <h4 className='mb-5 text-xl'>After Effects</h4>
+          <h4 className='mb-5 text-lg'>After Effects</h4>
         </div>
         <div className='max-w-[250px] flex flex-col justify-center items-center text-center'>
-          <div className='h-[110px] w-[110px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
+          <div className='h-[90px] w-[90px] bg-[#f9dbb3] rounded-lg mb-6 flex justify-center items-center text-[#393d3f]'>
             <SiAbletonlive className='w-12 h-12' />
           </div>
-          <h4 className='mb-5 text-xl'>Ableton Live</h4>
+          <h4 className='mb-5 text-lg'>Ableton Live</h4>
         </div>
       </div>
     </div>
