d014ad47e528c9967d433c30f87dc5b352f341ce
cl118 <le.chrisv@gmail.com>
2023-11-02 00:05:05 -0400

    animated solutions block

--- components/Solutions.tsx
+++ components/Solutions.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { solutionsData } from '../data/solutionsData'
 import SolutionsCard from './SolutionsCard'
+import { motion } from 'framer-motion'
 
 const Solutions = () => {
   return (
@@ -8,22 +9,46 @@ const Solutions = () => {
       {/* Title and description */}
       <div className='flex flex-col pl-4 pr-4 mt-20 sm:flex-row mb-14 lg:pr-0'>
         <div className='w-1/2'>
-          <h3 className='mb-5 text-2xl font-thin leading-none tracking-wider'>MY EXPERTISE</h3>
-          <h2 className='w-20 text-5xl font-bold tracking-wide'>Innovative Solutions</h2>
+          <motion.h3
+            className='mb-5 text-2xl font-thin leading-none tracking-wider'
+            initial={{ x: '-100px', opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: 'easeInOut', duration: 0.7, delay: 0 }}
+          >
+            MY EXPERTISE
+          </motion.h3>
+          <motion.h2
+            className='w-20 text-5xl font-bold tracking-wide'
+            initial={{ x: '-100px', opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.3 }}
+          >
+            Innovative Solutions
+          </motion.h2>
         </div>
-        <div className=' sm:w-1/2 lg:max-w-[470px] flex items-end'>
-          <p>
-            I will utilize my experience in graphics and multimedia editing to create pixel-perfect,
-            user-friendly platforms.
-          </p>
-        </div>
+        <motion.p
+          className=' sm:w-1/2 lg:max-w-[470px] flex items-end'
+          initial={{ y: '10px', opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.6 }}
+        >
+          I will utilize my experience in graphics and multimedia editing to create pixel-perfect,
+          user-friendly platforms.
+        </motion.p>
       </div>
       {/* Cards */}
       <div className='w-full max-w-[1170px] px-4 mx-auto relative flex flex-col lg:flex-row justify-between mb-12 lg:mb-28'>
-        <img
+        <motion.img
           src='./dot-grid-accent.svg'
           alt='/'
           className='absolute top-[-35px] right-[10px] lg:right-[-35px] h-[160px]'
+          initial={{ x: '100px', opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.6 }}
         />
         {solutionsData.map(({ title, icon, text }, index) => (
           <SolutionsCard key={index} title={title} icon={icon} text={text} index={index} />
--- components/SolutionsCard.tsx
+++ components/SolutionsCard.tsx
@@ -1,16 +1,27 @@
 import { FaCode } from 'react-icons/fa'
 import { ImVideoCamera } from 'react-icons/im'
 import { MdOutlinePalette } from 'react-icons/md'
+import { motion } from 'framer-motion'
 
 const SolutionsCard = ({ title, icon, text, index }) => {
   return (
-    <div
+    <motion.div
       key={index}
       className='bg-[#f0f0f0] rounded-lg lg:max-w-[320px] xl:max-w-[370px] mb-4 lg:mb-0 relavtive z-10'
+      initial={{ y: '20px', opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.9 }}
     >
       <div className='flex flex-col p-12 sm:flex-row lg:flex-col'>
         <div className='flex sm:flex-grow lg:flex-grow-0 sm:justify-center sm:items-center lg:justify-start lg:items-start'>
-          <div className='h-[112px] w-[112px] bg-[#f9dbb3] rounded-lg mb-12 sm:mb-0 lg:mb-24 flex justify-center items-center'>
+          <motion.div
+            className='h-[112px] w-[112px] bg-[#f9dbb3] rounded-lg mb-12 sm:mb-0 lg:mb-24 flex justify-center items-center'
+            initial={{ y: '20px', opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.9 }}
+          >
             {icon === 'FaCode' ? (
               <FaCode className='w-12 h-12' />
             ) : icon === 'MdOutlinePalette' ? (
@@ -18,14 +29,30 @@ const SolutionsCard = ({ title, icon, text, index }) => {
             ) : (
               <ImVideoCamera className='w-12 h-12' />
             )}
-          </div>
+          </motion.div>
         </div>
         <div className='flex flex-col w-full sm:w-2/3 lg:w-full'>
-          <h3 className='text-2xl font-bold lg:text-3xl xl:text-4xl lg:mb-5'>{title}</h3>
-          <p className='text-lg font-light'>{text}</p>
+          <motion.h3
+            className='text-2xl font-bold lg:text-3xl xl:text-4xl lg:mb-5'
+            initial={{ y: '20px', opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: 'easeInOut', duration: 0.7, delay: 0.9 }}
+          >
+            {title}
+          </motion.h3>
+          <motion.p
+            className='text-lg font-light'
+            initial={{ y: '20px', opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ease: 'easeInOut', duration: 0.7, delay: 1.2 }}
+          >
+            {text}
+          </motion.p>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 export default SolutionsCard
